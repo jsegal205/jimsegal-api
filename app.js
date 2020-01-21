f7907a6c24a4af80af7d0d4045b233757393a516
Jim Segal <hello@jimsegal.com>
2020-01-20 20:19:30 -0600

    update to fat arrow syntax

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const express = require("express");
 const port = process.env.PORT || 8001;
 const app = express();
 
-app.get("/", function(req, res) {
+app.get("/", (req, res) => {
   res.send(JSON.stringify({ Jim: "Segal" }));
 });
 
-app.listen(port, function() {
-  console.log(`Example app listening on port ${port} !`);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
 });
