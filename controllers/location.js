ec7630811bb191cae8d74d579fead39e6195a8a6
Jim Segal <hello@jimsegal.com>
2020-01-30 21:13:29 -0600

    add locations controller

---
+++ controllers/location.js
@@ -0,0 +1,25 @@
+const axios = require("axios");
+
+const getBy = async (lat, long) => {
+  try {
+    const API_URL = `https://www.mapquestapi.com/geocoding/v1/reverse?key=Y3zmO0lPUXVmIoPrDpMT1H2Nuu18mF5y`;
+    const res = await axios(`${API_URL}&location=${lat},${long}`);
+
+    const {
+      adminArea5: city,
+      adminArea3: state
+    } = res.data.results[0].locations[0];
+
+    return {
+      city,
+      state
+    };
+  } catch (err) {
+    console.error(err);
+    throw new Error(err);
+  }
+};
+
+module.exports = {
+  getBy
+};
