fb3e1fdc56e5f92498c8ab498558ceff3b1be377
Jim Segal <hello@jimsegal.com>
2020-02-05 16:41:33 -0600

    call model instead of pg dataclip

--- controllers/games.js
+++ controllers/games.js
@@ -1,23 +1,10 @@
-const axios = require("axios");
+const Games = require("../models/games");
 
 const getAll = async (req, res) => {
   try {
-    const apiResponse = await axios
-      .get(
-        "https://data.heroku.com/dataclips/donygkplrgieljfwbfisudzjmirb.json"
-      )
-      .catch(error => {
-        throw error;
-      });
-
-    res.json(
-      apiResponse.data.values.map(game => {
-        return { title: game[0], link: game[1], image: game[2] };
-      })
-    );
-  } catch (err) {
-    console.error(err);
-    throw err;
+    res.json(await Games.getAllGames());
+  } catch (error) {
+    throw error;
   }
 };
 
