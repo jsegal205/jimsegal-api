ba22fa16a233de954d583243065693f69cfadf18
Jim Segal <hello@jimsegal.com>
2020-02-06 22:00:32 -0600

    create game object

--- models/games.js
+++ models/games.js
@@ -1,10 +1,19 @@
 const db = require("../db/pg");
 
+class Game {
+  constructor(title, link, image) {
+    this.title = title || "";
+    this.link = link || "";
+    this.image = image || "";
+  }
+
+  isValid = () => [this.title, this.link, this.image].every(param => !!param);
+}
+
 const getAllGames = async () => {
   const results = await db.query("SELECT * FROM games ORDER BY name ASC");
-
-  return results.map(game => {
-    return { title: game.name, link: game.url, image: game.image_url };
+  return results.map(result => {
+    return new Game(result.name, result.url, result.image_url);
   });
 };
 
