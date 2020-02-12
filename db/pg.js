adb4b62cd32a4a31a9169f39487a28e4f4bcdbe6
Jim Segal <hello@jimsegal.com>
2020-02-11 21:00:20 -0600

    move pool setup to a function

--- db/pg.js
+++ db/pg.js
@@ -1,47 +1,47 @@
 const Pool = require("pg-pool");
 
-let pool;
+const configurePool = async () => {
+  if (process.env.HOST === "production") {
+    const url = require("url");
 
-if (process.env.HOST === "production") {
-  const url = require("url");
+    const params = url.parse(process.env.PG_DATABASE_URL);
+    const auth = params.auth.split(":");
 
-  const params = url.parse(process.env.PG_DATABASE_URL);
-  const auth = params.auth.split(":");
+    const config = {
+      user: auth[0],
+      password: auth[1],
+      host: params.hostname,
+      port: params.port,
+      database: params.pathname.split("/")[1],
+      ssl: {
+        rejectUnauthorized: false
+      }
+    };
 
-  const config = {
-    user: auth[0],
-    password: auth[1],
-    host: params.hostname,
-    port: params.port,
-    database: params.pathname.split("/")[1],
-    ssl: {
-      rejectUnauthorized: false
-    }
-  };
+    return new Pool(config);
+  }
 
-  pool = new Pool(config);
-}
+  if (process.env.HOST === "development") {
+    return new Pool({
+      user: process.env.PG_USER,
+      host: process.env.PG_HOST,
+      database: process.env.PG_DATABASE,
+      password: process.env.PG_PASSWORD,
+      port: 5432
+    });
+  }
 
-if (process.env.HOST === "development") {
-  pool = new Pool({
-    user: process.env.PG_USER,
-    host: process.env.PG_HOST,
-    database: process.env.PG_DATABASE,
-    password: process.env.PG_PASSWORD,
-    port: 5432
-  });
-}
+  throw ReferenceError("Pool connection not set up");
+};
 
 const query = async query => {
   if (!query) {
-    throw "no query provided";
+    throw ReferenceError("No query provided");
   }
 
-  if (!pool) {
-    throw "pool connection not set up";
-  }
-
+  const pool = await configurePool();
   const client = await pool.connect();
+
   try {
     const result = await client.query(query);
 
