de0847b500c95b98deb4225e349cffe361d267b2
Jim Segal <hello@jimsegal.com>
2020-05-09 19:12:48 -0500

    add slugify helper method

---
+++ utils/url.js
@@ -0,0 +1,11 @@
+const slugify = (input) => {
+  return input
+    .toLowerCase()
+    .replace(/[^a-z0-9 -]/g, "") // only allow alphanumeric, space, hyphen
+    .replace(/ +/g, "-") // reduce multiple space to single hyphen
+    .replace(/-+/g, "-"); // reduce multiple hyphen to single hyphen
+};
+
+module.exports = {
+  slugify,
+};
