f079c339009c867ee62e0b3f7394c32329bd33e9
Jim Segal <hello@jimsegal.com>
2020-05-12 20:26:19 -0500

    allow slug to be passed
    This is needed for db return as to not just return slugified title again
    in case the database ever changes and the title does not match slug

--- models/recipes.js
+++ models/recipes.js
@@ -4,9 +4,9 @@ const { slugify } = require("../utils/url");
 class Recipe {
   REQUIRED_FIELDS = ["title", "ingredients", "directions"];
 
-  constructor({ title, reference_link, ingredients, directions, notes }) {
+  constructor({ title, slug, reference_link, ingredients, directions, notes }) {
     this.title = title ? title.trim() : "";
-    this.slug = slugify(this.title);
+    this.slug = slug ? slug : slugify(this.title);
     this.referenceLink = reference_link ? reference_link.trim() : "";
     this.ingredients = ingredients ? ingredients.trim() : "";
     this.directions = directions ? directions.trim() : "";
