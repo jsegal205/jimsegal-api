96f2aac1ac266d7ca35b4543ff678624491f87ea
Jim Segal <hello@jimsegal.com>
2021-03-30 22:11:12 -0500

    depricate recipes create, make recipes gets pass through to admin.jimsegal.com

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -1,8 +1,14 @@
-const repo = require("../repos/recipes");
+const axios = require("axios");
 
 const getAll = async (req, res) => {
   try {
-    res.json(await repo.getAll());
+    const apiRes = await axios
+      .get("https://admin.jimsegal.com/recipes")
+      .catch((error) => {
+        throw error;
+      });
+
+    res.json(apiRes.data);
   } catch (error) {
     throw error;
   }
@@ -11,37 +17,20 @@ const getAll = async (req, res) => {
 const getBySlug = async (req, res) => {
   try {
     const { slug } = req.params;
-    const recipe = await repo.getBySlug(slug);
-    if (recipe) {
-      res.json(recipe);
-    } else {
-      res.status(404).send("Does not exist");
-    }
-  } catch (error) {
-    throw error;
-  }
-};
 
-const create = async (req, res) => {
-  try {
-    const { api_token } = req.headers;
+    const apiRes = await axios
+      .get(`https://admin.jimsegal.com/recipes/${slug}`)
+      .catch(({ name, message }) => {
+        res.json({ name, message });
+      });
 
-    if (!api_token || api_token !== process.env.AUTH_TOKEN) {
-      return res.status(403).send("Forbidden");
-    } else {
-      const { slug, ...permittedProps } = req.body;
-      const results = await repo.create(permittedProps);
-      const { persisted, ...retVal } = results;
-
-      res.status(persisted ? 201 : 400).json(retVal);
-    }
+    res.json(apiRes.data);
   } catch (error) {
     throw error;
   }
 };
 
 module.exports = {
-  create,
   getAll,
   getBySlug,
 };
--- tests/controllers/recipes.js
+++ tests/controllers/recipes.js
@@ -2,8 +2,8 @@ const chai = require("chai");
 const sinon = require("sinon");
 const expect = chai.expect;
 
-const Recipe = require("../../models/recipes");
-const repo = require("../../repos/recipes");
+// const Recipe = require("../../models/recipes");
+// const repo = require("../../repos/recipes");
 const controller = require("../../controllers/recipes");
 
 const { mockRequest, mockResponse } = require("./helpers");
@@ -23,148 +23,69 @@ describe("RecipeController", () => {
     it("should call to repo", async () => {
       const req = mockRequest();
       const res = mockResponse();
-      const stub = sandbox.stub(repo, "getAll");
+      // const stub = sandbox.stub(repo, "getAll");
+      const apiReturn = {
+        data: [
+          {
+            title: "title",
+            slug: "slug",
+            reference_link: "link",
+            ingredients: "ingredients",
+            directions: "directions",
+            notes: "notes",
+          },
+        ],
+      };
 
       await controller.getAll(req, res);
 
-      expect(stub.calledOnce).to.be.true;
+      // expect(stub.calledOnce).to.be.true;
+      expect(res.json.calledOnceWithExactly(apiReturn)).to.be.true;
     });
   });
 
   describe("getBySlug", () => {
     it("should call to repo", async () => {
       const slug = "test-slug";
-      const repoReturn = new Recipe({
-        title: "title",
-        slug,
-        reference_link: "link",
-        ingredients: "ingredients",
-        directions: "directions",
-        notes: "notes",
-      });
-      const repoStub = sandbox
-        .stub(repo, "getBySlug")
-        .withArgs(slug)
-        .returns(repoReturn);
+      const apiReturn = {
+        data: {
+          title: "title",
+          slug,
+          reference_link: "link",
+          ingredients: "ingredients",
+          directions: "directions",
+          notes: "notes",
+        },
+      };
+      // const repoStub = sandbox
+      //   .stub(repo, "getBySlug")
+      //   .withArgs(slug)
+      //   .returns(repoReturn);
 
       const req = mockRequest({ params: { slug } });
       const res = mockResponse();
       await controller.getBySlug(req, res);
 
-      expect(repoStub.calledOnce).to.be.true;
-      expect(res.json.calledOnceWithExactly(repoReturn)).to.be.true;
+      // expect(repoStub.calledOnce).to.be.true;
+      expect(res.json.calledOnceWithExactly(apiReturn)).to.be.true;
     });
 
     it("returns 404 status when nothing found", async () => {
       const slug = "test-slug";
 
-      const repoStub = sandbox
-        .stub(repo, "getBySlug")
-        .withArgs(slug)
-        .returns(null);
+      // const repoStub = sandbox
+      //   .stub(repo, "getBySlug")
+      //   .withArgs(slug)
+      //   .returns(null);
 
       const req = mockRequest({ params: { slug } });
       const res = mockResponse();
 
       await controller.getBySlug(req, res);
 
-      expect(repoStub.calledOnce).to.be.true;
+      // expect(repoStub.calledOnce).to.be.true;
       expect(res.status.calledOnceWithExactly(404)).to.be.true;
       expect(res.send.calledOnceWithExactly("Does not exist")).to.be.true;
-    });
-  });
-
-  describe("create", () => {
-    const validParams = {
-      title: "title",
-      reference_link: "link",
-      ingredients: "ingredients",
-      directions: "directions",
-      notes: "notes",
-    };
-    before(() => {
-      process.env.AUTH_TOKEN = "valid";
-    });
-
-    describe("when auth token is invalid", () => {
-      it("should not call to repo", async () => {
-        const repoStub = sandbox.stub(repo, "create").withArgs(validParams);
-        const req = mockRequest({
-          body: validParams,
-          headers: { api_token: "invalid" },
-        });
-        const res = mockResponse();
-
-        await controller.create(req, res);
-
-        expect(repoStub.calledOnce).to.be.false;
-        expect(res.status.calledOnceWithExactly(403)).to.be.true;
-        expect(res.send.calledOnceWithExactly("Forbidden")).to.be.true;
-      });
-    });
-
-    describe("when auth token is valid", () => {
-      it("does not send `slug` prop to repo", async () => {
-        const testParams = {
-          title: "title",
-          slug: "slug",
-        };
-        const repoStub = sandbox
-          .stub(repo, "create")
-          .returns({ persisted: true, matters: "not" });
-
-        const req = mockRequest({
-          body: testParams,
-          headers: { api_token: "valid" },
-        });
-        const res = mockResponse();
-
-        await controller.create(req, res);
-
-        const passedArg = repoStub.getCall(0).args[0];
-        expect(passedArg).to.deep.eq({ title: "title" });
-      });
-
-      describe("when data is persisted", () => {
-        it("should call to repo", async () => {
-          const recipe = new Recipe(validParams);
-          const expected = { recipe };
-          const repoStub = sandbox
-            .stub(repo, "create")
-            .returns({ persisted: true, ...expected });
-
-          const req = mockRequest({
-            body: validParams,
-            headers: { api_token: "valid" },
-          });
-          const res = mockResponse();
-          await controller.create(req, res);
-
-          expect(repoStub.calledOnce).to.be.true;
-          expect(res.status.calledOnceWithExactly(201)).to.be.true;
-          expect(res.json.calledOnceWithExactly(expected)).to.be.true;
-        });
-      });
-
-      describe("when data is not persisted", () => {
-        it("should call to repo", async () => {
-          const expected = { message: "data not persisted" };
-          const repoStub = sandbox
-            .stub(repo, "create")
-            .returns({ persisted: false, ...expected });
-
-          const req = mockRequest({
-            body: validParams,
-            headers: { api_token: "valid" },
-          });
-          const res = mockResponse();
-          await controller.create(req, res);
-
-          expect(repoStub.calledOnce).to.be.true;
-          expect(res.status.calledOnceWithExactly(400)).to.be.true;
-          expect(res.json.calledOnceWithExactly(expected)).to.be.true;
-        });
-      });
     });
   });
 });
