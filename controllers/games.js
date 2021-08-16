0ed669b835e28b796a27c93c99bff8ab54243ae4
Jim Segal <hello@jimsegal.com>
2021-08-16 15:57:24 -0500

    change from internal PG call to admin.jimsegal.com

--- controllers/games.js
+++ controllers/games.js
@@ -1,8 +1,22 @@
-const repo = require("../repos/games");
+const axios = require("axios");
+const { adminUrlBase } = require("../utils/constants");
 
 const getAll = async (req, res) => {
   try {
-    res.json(await repo.getAll());
+    await axios
+      .get(`${adminUrlBase}/games`)
+      .then(({ data }) => {
+        res.json(
+          data.map((game) => ({
+            title: game.name,
+            link: game.url,
+            image: game.image_url,
+          }))
+        );
+      })
+      .catch(({ message, name }) => {
+        res.json({ message, name });
+      });
   } catch (error) {
     throw error;
   }
