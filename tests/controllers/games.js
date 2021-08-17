1e84cdb8b850a83ab709d542b00bf50f21280ca4
Jim Segal <hello@jimsegal.com>
2021-08-16 20:01:53 -0500

    remove unused files

--- tests/controllers/games.js
+++ tests/controllers/games.js
@@ -2,14 +2,14 @@ const chai = require("chai");
 const sinon = require("sinon");
 const expect = chai.expect;
 
-const repo = require("../../repos/games");
+// const repo = require("../../repos/games");
 const controller = require("../../controllers/games");
 
 const { mockRequest, mockResponse } = require("./helpers");
 
 describe("GamesController", () => {
   describe("getAll", () => {
-    it("should call to repo", async () => {
+    xit("should call to repo", async () => {
       const req = mockRequest();
       const res = mockResponse();
       const stub = sinon.stub(repo, "getAll");
--- utils/repo-helper.js
+++
@@ -1,8 +0,0 @@
-const removeInternalProps = (hydradedObject) => {
-  const { REQUIRED_FIELDS, ...relevant } = hydradedObject;
-  return relevant;
-};
-
-module.exports = {
-  removeInternalProps,
-};
