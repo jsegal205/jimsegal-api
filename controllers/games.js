3bbf225aab0bd2fe5ebf249e24a4feeb171ee9ed
Jim Segal <hello@jimsegal.com>
2021-10-19 21:21:19 -0500

    remove sort

--- controllers/games.js
+++ controllers/games.js
@@ -7,16 +7,11 @@ const getAll = async (req, res) => {
       .get(`${adminUrlBase}/games`)
       .then(({ data }) => {
         res.json(
-          data
-            .map((game) => ({
-              title: game.name,
-              link: game.url,
-              image: game.image_url,
-            }))
-            .sort((current, next) => {
-              if (current.title == next.title) return 0;
-              return current.title > next.title ? 1 : -1;
-            })
+          data.map((game) => ({
+            title: game.name,
+            link: game.url,
+            image: game.image_url,
+          }))
         );
       })
       .catch(({ message, name }) => {
