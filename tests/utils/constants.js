b585ccc8e6244d5dcceb4d1441496d4444d9f88c
Jim Segal <hello@jimsegal.com>
2022-11-27 10:26:34 -0600

    update admin urls

--- tests/utils/constants.js
+++ tests/utils/constants.js
@@ -3,7 +3,7 @@ const assert = require("assert");
 const { adminUrlBase } = require("../../utils/constants");
 
 describe("Constants.adminUrlBase", () => {
-  it("should return https://admin.jimsegal.com", () => {
-    assert.strictEqual(adminUrlBase, "https://admin.jimsegal.com");
+  it("should return https://railway-admin.jimsegal.com/api", () => {
+    assert.strictEqual(adminUrlBase, "https://railway-admin.jimsegal.com/api");
   });
 });
--- utils/constants.js
+++ utils/constants.js
@@ -1,6 +1,9 @@
-const adminUrlBase = "https://admin.jimsegal.com";
+const adminUrlBase = "https://railway-admin.jimsegal.com/api";
 // const adminUrlBase = 'http://localhost:1337' // local
+
+const adminUrlQueryParams = "populate=%2A&pagination[pageSize]=100";
 
 module.exports = {
   adminUrlBase,
+  adminUrlQueryParams,
 };
